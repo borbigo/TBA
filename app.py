69aa90fd92607a764fef6928a7b939d61cd1345e
tampalloor <tampalloor@gmail.com>
2023-11-27 14:51:24 -0800

    test change

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def schedule_reservations():
 def activity():
   return render_template('activity.html')
 
-# create and view employees
+# create and view employees --wefwoeijfowijefojweofjiwoijefwieofjowjiefw
 # view by group (i.e. all front desk, all trainers)
 #! unfinished
 @app.route('/employees') 
