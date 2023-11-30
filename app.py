b90b50dbfe20608830a780df50d12efee53f17e1
Thomas <63935525+tampalloor@users.noreply.github.com>
2023-11-29 16:26:21 -0800

    Updated app.py for employee

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, jsonify, session
+from flask import Flask, render_template, jsonify, session, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text #Thoams Ampallor
+from sqlalchemy import text 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:TotalBodyAthletics@gym-db.c5fopmlkwx7y.us-east-1.rds.amazonaws.com:3306/TBAGYM'
@@ -31,33 +31,38 @@ def find_club():
   session['all_data'] = data_list
   return render_template('find_club.html', all_data = data_list) 
 
-# space/room availability -> what's booked out for classes
-#! unfinished
-@app.route('/schedule_reservations')
-def schedule_reservations():
-  return render_template('schedule_reservations.html')
-
-# covers recent gym activity/workouts
-#! unfinished
+# displays available classes for users -> needs to filter by day and/or time and/or branch
+#TODO in progress - starting html file 
 @app.route('/activity') 
 def activity():
+  data_list = get_data("CLASS")
+  session['all_data'] = data_list
   return render_template('activity.html')
+
+# active reservations
+#! unfinished
+@app.route('/reservations')
+def reservations():
+  return render_template('reservations.html')
 
 # create and view employees 
 # view by group (i.e. all front desk, all trainers)
-#! unfinished
 @app.route('/employees') 
 def employees():
-  return render_template('employees.html')
+  data_list = get_data("EMPLOYEE")
+  session['all_data'] = data_list
+  return render_template('employees.html', all_data = data_list)
 
 # view amenities specifically rooms
-#! unfinished
+#TODO in progress - starting html file 
 @app.route('/amenities') 
 def amenities():
-  return render_template('amenities.html')
+  data_list = get_data("ROOM")
+  session['all_data'] = data_list
+  return render_template('amenities.html', all_data = data_list)
 
 # view equipment (name, type, weight)
-#TODO in progress
+#TODO in progress - data is dumping formatting is still needed
 @app.route('/equipment') 
 def equipment():
   data_list = get_data("EQUIPMENT")
@@ -65,12 +70,32 @@ def equipment():
   return render_template('equipment.html', all_data = data_list)
 
 # just a merchandise page
-#TODO: in progress
-@app.route('/merchandise') 
+@app.route('/merchandise', methods=['GET', 'POST']) 
 def merchandise():
-  data_list = get_data("GEAR")
-  session['all_data'] = data_list
-  return render_template('merchandise.html', all_data = data_list)
+  # Fetch distinct locations from the GYM table
+    locations_query = text('SELECT DISTINCT Location FROM GYM')
+    locations_result = db.session.execute(locations_query)
+    all_locations = [row[0] for row in locations_result]
+
+    # Default location (first one) for initial rendering
+    selected_location = all_locations[0] if all_locations else None
+
+    if request.method == 'POST':
+        # If form is submitted, update the selected location
+        selected_location = request.form.get('location')
+
+    # Fetch gear data based on the selected location
+    gear_query = text(f'SELECT * FROM GEAR WHERE Branch = "{selected_location}"')
+    gear_result = db.session.execute(gear_query)
+
+    # Print debug information
+    # print(f"Selected Location: {selected_location}")
+
+    # Print the structure of the result set
+    # print(f"Columns: {gear_result.keys()}")
+
+    return render_template('merchandise.html', all_locations=all_locations, selected_location=selected_location, gear_columns=gear_result.keys(), gear_result=gear_result)
+
 
 # shows off member information (omits personal info)
 @app.route('/community') 
